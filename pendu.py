40fc284c93ef97d50b855480f03a956835604626
Ubuntutute <158178339+Ubuntutute@users.noreply.github.com>
2024-01-30 16:50:22 +0100

    Update pendu.py
    changement du nom des variables

--- pendu.py
+++ pendu.py
@@ -1,50 +1,50 @@
 from getpass import getpass
 mot = getpass(" joueur 1 veuillez saisir le mot secret: ")
-tentatives = 7
-affichage = ""
-lettres_trouvees = ""
+TENTATIVES = 7
+AFFICHAGE_DU_PENDU = ""
+LETTRES_TROUVEES = ""
 
 for l in mot:
-  affichage = affichage + "_ "
+  AFFICHAGE_DU_PENDU = AFFICHAGE_DU_PENDU + "_ "
 
 print(">> Bienvenue dans le pendu <<")
 
-while tentatives > 0:                                       #ici on dit que temps que le nombre de tentative restante est supérieur à 0 alors le jeu continue
-  print("\nMot à deviner : ", affichage)
+while TENTATIVES > 0:                                       #ici on dit que temps que le nombre de tentative restante est supérieur à 0 alors le jeu continue
+  print("\nMot à deviner : ", AFFICHAGE_DU_PENDU)
   proposition = input("proposez une lettre : ")[0:1].lower()
 
  
   if proposition in mot:                                    #on définit ici que si la proposition du joueur est une partie du mot alors on lui dis que c'est bon
-      lettres_trouvees = lettres_trouvees + proposition
+      LETTRES_TROUVEES = LETTRES_TROUVEES + proposition
       print("-> Bien vu!")
   else:                                                     #sinon ici on affiche que c'est raté et que de faites on affiche le pendu
-    tentatives = tentatives - 1
+    TENTATIVES = TENTATIVES - 1
     print("-> Nope\n")
-    if tentatives==0:
+    if TENTATIVES==0:
         print(" ==========Y= ")
-    if tentatives<=1:
+    if TENTATIVES<=1:
         print(" ||/       |  ")
-    if tentatives<=2:
+    if TENTATIVES<=2:
         print(" ||        0  ")
-    if tentatives<=3:
+    if TENTATIVES<=3:
         print(" ||       /|\ ")
-    if tentatives<=4:
+    if TENTATIVES<=4:
         print(" ||       /|  ")
-    if tentatives<=5:                    
+    if TENTATIVES<=5:                    
         print("/||           ")
-    if tentatives<=6:
+    if TENTATIVES<=6:
         print("==============\n")
 
 #la on affiche le nombre de lettres qu'il va falloir deviner en mettant des _ popur chaque lettre
-  affichage = ""
+  AFFICHAGE_DU_PENDU = ""
   for x in mot:
-      if x in lettres_trouvees:
-          affichage += x + " "
+      if x in LETTRES_TROUVEES:
+          AFFICHAGE_DU_PENDU += x + " "
       else:
-          affichage += "_ "
+          AFFICHAGE_DU_PENDU += "_ "
 
 #La on définit que si il n'y a plus de _ la partie se finit
-  if "_" not in affichage:
+  if "_" not in AFFICHAGE_DU_PENDU:
       print(">>> Gagné! <<<")
       break
      
